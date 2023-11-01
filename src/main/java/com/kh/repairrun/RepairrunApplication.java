0e80955857b4171c31980bba558268ef684d36e5
hjck4433 <hjck4433@gmail.com>
2023-11-01 17:23:59 +0900

    repair run back start!

---
+++ src/main/java/com/kh/repairrun/RepairrunApplication.java
@@ -0,0 +1,13 @@
+package com.kh.repairrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairrunApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kh/vo/ReviewVO.java
@@ -0,0 +1,4 @@
+package com.kh.vo;
+
+public class ReviewVO {
+}
