5b695069867b51f36448c4738208c4b62dda5f1f
ljw2669 <dlwodnnjs@gmail.com>
2023-11-03 14:49:01 +0900

    CouponVO, ServiceVO 추가

--- src/main/java/com/kh/vo/CouponVO.java
+++ src/main/java/com/kh/vo/CouponVO.java
@@ -1,4 +1,24 @@
 package com.kh.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CouponVO {
+    private String couponType; // 쿠폰 종류 (예: 할인 쿠폰, 무료 배송 쿠폰 등)
+    private int discountAmount; // 할인 금액
+    private int minPrice; // 최소 금액
+    private String expDate; // 유효 기간
+
+    public CouponVO(String couponType, int discountAmount, int minPrice, String expDate) {
+        this.couponType = couponType;
+        this.discountAmount = discountAmount;
+        this.minPrice = minPrice;
+        this.expDate = expDate;
+    }
 }
--- src/main/java/com/kh/vo/ServiceVO.java
+++ src/main/java/com/kh/vo/ServiceVO.java
@@ -1,4 +1,30 @@
 package com.kh.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class ServiceVO {
+    private String faqTitle;
+    private String faqContent;
+    private String noticeTitle;
+    private String noticeContent;
+    private String noticeDate;
+
+    // FAQ
+    public ServiceVO(String faqTitle, String faqContent) {
+        this.faqTitle = faqTitle;
+        this.faqContent = faqContent;
+    }
+    // 공지사항
+    public ServiceVO(String noticeTitle, String noticeContent, String noticeDate) {
+        this.noticeTitle = noticeTitle;
+        this.noticeContent = noticeContent;
+        this.noticeDate = noticeDate;
+    }
 }
