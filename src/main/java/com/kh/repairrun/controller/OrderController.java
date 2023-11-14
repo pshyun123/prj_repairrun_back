a3b184011edc04165e651ce595ebd70c43777eef
pshyun123 <pshyun7204@gmail.com>
2023-11-14 17:13:22 +0900

    경로수정 박소현

--- src/main/java/com/kh/repairrun/controller/OrderController.java
+++ src/main/java/com/kh/repairrun/controller/OrderController.java
@@ -17,7 +17,7 @@ public class OrderController {
     @PostMapping("/detail")
     // 매개변수는 () 사용 ()안의 값에는 리액트에서 넘어오는 값, 키와 밸류의 자료형 선언^^
     public ResponseEntity<List<String>> detailItem (@RequestBody Map<String,String> selectItem) {//리스트로 받을거라고 선언
-        String selItem = selectItem.get("detail"); // 리액트에서 받아온 정보를 selItem으로 사용하겠다고 선언
+        String selItem = selectItem.get("repairItem"); // 리액트에서 받아온 정보를 repairItem으로 사용하겠다고 선언
         OrderDAO dao = new OrderDAO(); // 객체 생성
         List<String> result = dao.orderSelect(selItem); // 리스트로 값을 반환
         return new ResponseEntity<>(result, HttpStatus.OK); // 반환해주는 변수 이름이 result,HttpStatus 200일 경우에만 반환
@@ -27,7 +27,6 @@ public class OrderController {
 
 //    메소드 명은 소문자 함수도..
     //get 방식은 - 주소창에 정보가 보임, post 방식은 바디안에 숨어서
-
     @PostMapping("/ptnlist")
     public ResponseEntity<List<Map<String,Object>>> ptnSelList(@RequestBody Map<String,String> selectDetail) {
         String selDetail = selectDetail.get("detail");
@@ -38,7 +37,8 @@ public class OrderController {
 
     @PostMapping("/payment")
     public ResponseEntity<List<Map<String,Object>>> paymentProcess(@RequestBody Map<String, String > process){
-        String processP = process.get("name");
+        String processP = process.get("userId");
+        System.out.println(processP);
         OrderDAO dao = new OrderDAO();
         List<Map<String,Object>> result = dao.payment(processP);
         return new ResponseEntity<>(result,HttpStatus.OK);
--- src/main/java/com/kh/repairrun/dao/OrderDAO.java
+++ src/main/java/com/kh/repairrun/dao/OrderDAO.java
@@ -105,35 +105,35 @@ public class OrderDAO {
         try{
             conn = Common.getConnection();
             String sql = "SELECT " +
-                    "M.USER_NAME, " +
-                    "M.USER_PHONE, " +
-                    "M.USER_ADDR, " +
-                    "CUT.COUPON_TYPE_FK " +
+                    "CU.COUPON_TYPE_FK, " +
+                    "CT.DISCOUNT_AMOUNT, " +
+                    "CASE " +
+                    "WHEN CT.MIN_PRICE IS NOT NULL THEN CT.MIN_PRICE " +
+                    "ELSE 0 " +
+                    "END AS MIN_PRICE " +
                     "FROM " +
-                    "MEMBER_TB M " +
+                    "COUPON_USER_TB CU " +
                     "JOIN " +
-                    "COUPON_USER_TB CUT ON M.USER_ID_PK = CUT.USER_ID_FK " +
+                    "COUPON_TYPE_TB CT ON CU.COUPON_TYPE_FK = CT.COUPON_TYPE_PK " +
                     "WHERE " +
-                    "M.USER_NAME = ? ";
-
+                    "CU.END_DATE > SYSDATE " +
+                    "AND CU.USER_ID_FK = ?";
 
             pstmt = conn.prepareStatement(sql);// 보낼 쿼리문을 준비
             //?의 값을 지정
             pstmt.setString(1,paySum); //
-
             rs = pstmt.executeQuery();//
 
             while (rs.next()) { // 값이 있는 동안 한줄씩 반환
-                String userName = rs.getString("USER_NAME");
-                String userPhone = rs.getString("USER_PHONE");
-                String userAddr = rs.getString("USER_ADDR");
+
                 String couponType = rs.getString("COUPON_TYPE_FK");
+                String discountAmount = rs.getString("DISCOUNT_AMOUNT");
+                String minPrice = rs.getString("MIN_PRICE");
                 // 각 정보를 리스트에 추가
                 Map<String,Object> couponMap = new HashMap<>();
-                couponMap.put("name", userName);
-                couponMap.put("phone", userPhone);
-                couponMap.put("addr", userAddr);
                 couponMap.put("coupon", couponType);
+                couponMap.put("discount", discountAmount);
+                couponMap.put("minprice", minPrice);
                 list.add(couponMap);
             }
             Common.close(rs);
